dd2c038ddcafefae206d156d2012e45a11e2ca92
Ethan Meyers <ewm1230@gmail.com>
2022-07-15 16:02:54 -0500

    Added file source as email

--- main.py
+++ main.py
@@ -3,12 +3,82 @@
 # @Phone: 847-212-2264
 # @Created Date: 07/13/2022
 
+import os
+import sys
 import pandas as pd
 import pyodbc as pyodbc
+import win32com.client
+import zipfile
+from pathlib import Path
+
+
+mailTo = 'emeyers@whimsytrucking.com'
+inboxEmail = 'emeyers@whimsytrucking.com'
+
+
+def sendFailureEmail(reason: str):
+    olApp = win32com.client.Dispatch("Outlook.Application")
+    olNS = olApp.GetNamespace("MAPI")
+
+    mailItem = olApp.CreateItem(0)
+    mailItem.Subject = 'Automatic Task Failure'
+    mailItem.BodyFormat = 1
+    mailItem.Body = "This is an automated email informing you that the task 'Default_TEST " \
+                    "Upload' has failed for the following reason:\n\"{}\"\n\n".format(reason)
+    mailItem.To = mailTo
+    mailItem._oleobj_.Invoke(*(64209, 0, 8, 0, olNS.Accounts.Item(inboxEmail)))
+
+    mailItem.Save()
+    mailItem.Send()
+
+    sys.exit(1)
+
+
+def getFileFromEmail():
+    dir_path = '%s\\DefaultTestAuto\\' % os.environ['APPDATA']
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    olApp = win32com.client.Dispatch("Outlook.Application")
+    olNS = olApp.GetNamespace("MAPI")
+
+    inbox = olNS.GetDefaultFolder(6)
+    item = None
+    for messages in inbox.Items:
+        if 'AUTO DEFAULT TEST' in messages.Subject:
+            item = messages
+            break
+
+    if item is None:
+        print('File Not Found')
+        sendFailureEmail('Default_TEST email not found in inbox')
+    else:
+        print(item.Subject)
+        dir_path = Path(dir_path)
+        # dir_path = dir_path / str('Default_TEST')
+        # dir_path.mkdir(parents=True, exist_ok=True)
+        csvPath = []
+        for attachment in item.Attachments:
+            attachment.SaveAsFile(dir_path / str(attachment))
+
+        for i in os.listdir(dir_path):
+            if '.zip' in i:
+                with zipfile.ZipFile(dir_path / i, 'r') as zip_ref:
+                    zip_ref.extractall(dir_path)
+                os.remove(dir_path / i)
+
+        for i in os.listdir(dir_path):
+            csvPath.append(dir_path / i)
+
+        if '.csv' in csvPath[0]:
+            return csvPath[0]
+        else:
+            sendFailureEmail('Unknown attachment found')
+        # TODO: Delete mail item when done (MailItem.Delete)
 
 
 # @Description: Returns a pandas dataframe consisting of some old and some new records from a .csv file.
-def getFile():
+def getFileFromLocal():
     FilePath = r'C:\Users\emeyers\Desktop\default_test_2.csv'
     df = pd.read_csv(FilePath, header=0, encoding='unicode_escape')
     df.replace({pd.NaT: None}, inplace=True)
@@ -26,16 +96,15 @@ def getFile():
 
 
 # @Description: Inserts new records into designated microsoft access database
-def updateAccess():
+def updateAccess(filePath):
     # Setup
-    filePath = r"C:\Users\emeyers\Desktop\EthanAccess.accdb"
     driver = pyodbc.dataSources()
     driver = driver['MS Access Database']
     connection = pyodbc.connect(driver=driver, dbq=filePath)
     cursor = connection.cursor()
     tableName = 'Pick Up 2022 Cont'
 
-    df = getFile()
+    df = getFileFromEmail()
     lastUpdate = 0
     maxItem = 0
 
@@ -65,8 +134,12 @@ def updateAccess():
 
     print('Commit in progress...')
     connection.commit()
+    # TODO: Remove used CSV file
     print('Finished')
 
 
 if __name__ == '__main__':
-    updateAccess()
+    try:
+        updateAccess(getFileFromEmail())
+    except Exception as e:
+        sendFailureEmail(e)
